3c697014cb964b7a17d4c6ba216ea0817acd8673
Christopher Barnes <christopher@barnes.biz>
2025-10-12 21:26:20 +0200

    chore: update eslint-plugin-react-hooks

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,50 @@
+// eslint.config.js
 import js from "@eslint/js";
 import reactDom from "eslint-plugin-react-dom";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import reactX from "eslint-plugin-react-x";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  { ignores: ["dist", "eslint.config.*"] },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+
   {
     files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      reactDom.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-      reactX.configs["recommended-typescript"],
-      tseslint.configs.recommended,
-      tseslint.configs.recommendedTypeChecked,
-      tseslint.configs.strictTypeChecked,
-      tseslint.configs.stylisticTypeChecked,
-    ],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-dom": reactDom,
+      "react-refresh": reactRefresh,
+      "react-x": reactX,
+      "@typescript-eslint": tseslint.plugin,
+    },
+
     languageOptions: {
       ecmaVersion: 2025,
       globals: globals.browser,
       parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json', './tsconfig.test.json', './tsconfig.json'],
+        project: [
+          "./tsconfig.node.json",
+          "./tsconfig.app.json",
+          "./tsconfig.test.json",
+          "./tsconfig.json",
+        ],
         tsconfigRootDir: import.meta.dirname,
       },
+    },
+
+    rules: {
+      ...(reactHooks.configs["recommended-latest"]?.rules ?? {}),
+      ...(reactRefresh.configs?.vite?.rules ?? {}),
+      ...(reactX.configs["recommended-typescript"]?.rules ?? {}),
     },
   },
 ]);
