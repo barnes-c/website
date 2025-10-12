098c600c25ac48cf34428f461d145de35f6fd3c0
Christopher Barnes <68067715+barnes-c@users.noreply.github.com>
2025-10-12 19:27:48 +0200

    style(linter): add type-aware checks and react lint rules (#3)
    * fix: ignore dist folder
    
    * feat: add bun-types
    
    * style(lint): add type-aware checks
    
    * fix: add tsconfig to eslint config
    
    * extract test in extra job

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+const el = document.getElementById('root')
+if (!el) throw new Error('Root element #root not found')
+
+createRoot(el).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
-
-
