0c8f34ded9537ac979b8b96ef6ea536e572df0eb
Christopher Barnes <christopher@barnes.biz>
2025-10-12 18:40:56 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import tailwindcss from "@tailwindcss/vite"
-import react from '@vitejs/plugin-react-swc'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(),
-
-  tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    target: "esnext",
+    sourcemap: true,
+  },
+  optimizeDeps: { esbuildOptions: { target: "esnext" } }
 });
