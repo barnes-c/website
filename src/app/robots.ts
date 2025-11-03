81c87cdf2e06ee988bf4808e1a916ac4f8c0d564
Christopher Barnes <christopher@barnes.biz>
2025-11-03 15:34:47 +0100

    fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,5 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             disallow: ['/'],
         },
-        sitemap: 'https://barnes.biz/sitemap.xml',
     }
 }
